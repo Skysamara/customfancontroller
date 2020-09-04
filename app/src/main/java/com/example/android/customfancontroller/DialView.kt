2d53267776370705628b0c6fe7b023adb111533b
skysamara <skysamara@gmail.com>
2020-09-04 17:41:05 +0300

    Add custom attributes

--- app/src/main/java/com/example/android/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
+import java.time.format.ResolverStyle
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
@@ -41,12 +43,21 @@ class DialView @JvmOverloads constructor(
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        textSize = 55.0f
+        textSize = 30.0f
         typeface = Typeface.create( "", Typeface.BOLD)
     }
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor = 0
+    private var fanSpeedMaxColor = 0
 
     init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.DialView) {
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1, 0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+            fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
+        }
     }
 
     override fun performClick(): Boolean {
@@ -75,7 +86,15 @@ class DialView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         // Set dial background color to green if selection not off.
-        paint.color = if(fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+//        paint.color = if(fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        paint.color = when(fanSpeed) {
+            FanSpeed.OFF -> Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSpeedMaxColor
+        }  as Int
+
+
         canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
 
         // Draw the indicator circle.
